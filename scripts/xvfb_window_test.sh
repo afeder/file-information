554b2f928e0b052dd81d4a9b369706064c12e870
Anders Feder <anders.feder.83@gmail.com>
2025-06-05 15:15:29 +0200

    Add commentary on environment.

--- scripts/xvfb_window_test.sh
+++ scripts/xvfb_window_test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+# Note that it may be necessary to install `dbus-x11` to avoid warnings about
+# the session bus.
+
 XVFB_DISPLAY=:99
 
 cleanup() {
@@ -21,6 +24,8 @@ export DISPLAY="$XVFB_DISPLAY"
 # Force GTK apps to use the X11 backend so they don't try to default to Wayland
 # where available instead of connecting to Xvfb.
 export GDK_BACKEND=x11
+# This may be necessary to prevent attempts to use hardware graphics.
+#export LIBGL_ALWAYS_SOFTWARE=1
 
 ./target/release/file-information README.md &
 APP_PID=$!
