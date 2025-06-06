235abfcf3233072576645ee659634b2c96f33440
Anders Feder <anders.feder.83@gmail.com>
2025-06-06 17:00:51 +0200

    Add comments.

--- tests/bootstrap_environment.sh
+++ tests/bootstrap_environment.sh
@@ -1,6 +1,14 @@
+# This script is designed to bootstrap the environment for testing. It assumes a
+# minimal Debian/Ubuntu base environment.
+
 apt-get update
+
+# Install main app dependencies.
 apt-get install -y libglib2.0-dev libcairo2-dev libpango1.0-dev libgdk-pixbuf2.0-dev libgtk-4-dev libadwaita-1-dev libtracker-sparql-3.0-dev pkg-config
+
+# Install testing dependencies.
 apt-get install -y xvfb dbus-x11 xdotool imagemagick
 apt-get install -y tracker gedit xdg-utils
 xdg-mime default org.gnome.gedit.desktop text/plain
+
 echo "ENVIRONMENT SETUP COMPLETE."
